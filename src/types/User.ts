76647bbed45342f4099bb8ad6ee73196aadf7b68
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-03 21:25:24 +0300

    feat: add user type

---
+++ src/types/User.ts
@@ -0,0 +1,29 @@
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+type Geo = {
+  lat: number;
+  lng: number;
+};
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipCode: string;
+  geo: Geo;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  userName: string;
+  email: string;
+  address: Address;
+  company: Company;
+  website: string;
+  phone: string;
+};
