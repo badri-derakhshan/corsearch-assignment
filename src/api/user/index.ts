3d07d9b40714a1b75c12bb188a4929d63562e127
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-03 21:26:03 +0300

    feat: add user api services

---
+++ src/api/user/index.ts
@@ -0,0 +1,53 @@
+import { get } from '@/services/api';
+import { User } from '@/types/User';
+
+type UserResponse = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: number;
+      lng: number;
+    };
+  };
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+  website: string;
+  phone: string;
+};
+
+const transformUserResponse = (user: UserResponse): User => {
+  return {
+    name: user.name,
+    email: user.email,
+    id: user.id,
+    userName: user.username,
+    address: {
+      city: user.address.city,
+      geo: user.address.geo,
+      street: user.address.street,
+      suite: user.address.suite,
+      zipCode: user.address.zipcode
+    },
+    company: user.company,
+    phone: user.phone,
+    website: user.website
+  };
+};
+
+const GET_ALL_USERS = '/users';
+
+export const getAllUsers = () => {
+  return get<UserResponse[]>(GET_ALL_USERS).then((users) => {
+    return users.map(transformUserResponse);
+  });
+};
