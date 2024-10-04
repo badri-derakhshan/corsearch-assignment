cb842ba58db74b55cdd441ddfe80f1d1fc84d32b
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-05 00:19:40 +0300

    feat: add layuot support to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
+import { TooltipProvider } from '@/components/ui/tooltip';
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  const Page = getLayout(<Component {...pageProps} />);
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>{Page}</TooltipProvider>
+    </QueryClientProvider>
+  );
 }
