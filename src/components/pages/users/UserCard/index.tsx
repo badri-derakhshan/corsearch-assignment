528c17b95f3eddec93b7f52149632445e6677ffd
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-05 00:21:38 +0300

    feat: add user card component

---
+++ src/components/pages/users/UserCard/index.tsx
@@ -0,0 +1,85 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+import Link from 'next/link';
+import { Mail, Phone, MapPin, User2, ExternalLink } from 'lucide-react';
+import {
+  creatWebsiteUrl,
+  createGoogleMapUrl,
+  createMailtoUrl,
+  createPhoneUrl
+} from '@/utils/url';
+import type { User } from '@/types';
+import styles from './style.module.scss';
+
+type Props = { user: User };
+const UserCard = ({ user }: Props) => {
+  const { id, name, email, phone, website, address } = user;
+  return (
+    <Card key={id}>
+      <CardHeader>
+        <CardTitle className={styles['cardHeader__titleContainer']}>
+          <User2 />
+          <p className={styles['cardHeader__titleContainer__title']}>{name}</p>
+        </CardTitle>
+        <Link
+          className={styles['cardHeader__website']}
+          target='_blank'
+          href={creatWebsiteUrl(website)}
+        >
+          <CardDescription>{website}</CardDescription>
+          <ExternalLink size={15} />
+        </Link>
+      </CardHeader>
+      <CardContent className={styles.cardContent}>
+        <small className={styles['cardContent__addressText']}>
+          {' '}
+          City : {address.street}
+        </small>
+        <small className={styles['cardContent__addressText']}>
+          Address : {address.street} , {address.suite}
+        </small>
+        <small className={styles['cardContent__addressText']}>
+          {' '}
+          PostalCode : {address.zipCode}
+        </small>
+      </CardContent>
+      <CardFooter className={styles.cardFooter}>
+        <Link
+          className={styles['cardFooter__titleContainer']}
+          target='_blank'
+          title='show on map'
+          href={createGoogleMapUrl(address.geo)}
+        >
+          <MapPin size={15} className={styles['cardFooter__icon']} />
+          <p>Location</p>
+        </Link>
+
+        <Link
+          href={createPhoneUrl(phone)}
+          className={styles['cardFooter__titleContainer']}
+        >
+          <Phone size={15} className={styles['cardFooter__icon']} />
+          <p>{phone}</p>
+        </Link>
+
+        <Link
+          target='_blank'
+          title='send email'
+          href={createMailtoUrl(email)}
+          className={styles['cardFooter__titleContainer']}
+        >
+          <Mail size={15} className={styles['cardFooter__icon']} />
+          <p>{email}</p>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export { UserCard };
