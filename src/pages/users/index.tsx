acfb994181b632de42e7b207da79c7130032612b
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-05 00:22:24 +0300

    feat: add users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,54 @@
 import { getAllUsers } from '@/api/user';
-import { User } from '@/types';
-import { useEffect, useState } from 'react';
+import { DashboardLayout } from '@/components/layouts/DashboardLayout';
+import { FilterContainer } from '@/components/pages/users/FilterContainer';
+import { UserCard } from '@/components/pages/users/UserCard';
+import type { User } from '@/types';
+import { filterBy, FilterByParams, sortBy, SortByParams } from '@/utils/array';
+import { useQuery } from '@tanstack/react-query';
+import { ReactElement, useMemo, useState } from 'react';
+import { NextPageWithLayout } from '../_app';
+import style from './style.module.scss';
 
-export default function Users() {
-  const [users, setUsers] = useState<User[]>();
-  useEffect(() => {
-    getAllUsers().then(setUsers);
-  }, []);
+const Users: NextPageWithLayout = () => {
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: getAllUsers
+  });
+
+  const [filter, setFilter] = useState<FilterByParams<User>>({
+    filterKey: 'name',
+    search: ''
+  });
+
+  const [sort, setSort] = useState<SortByParams<User> | undefined>();
+
+  const modifiedUsers = useMemo(() => {
+    if (!data) return;
+    const filteredData = filter ? filterBy(data, filter) : data;
+
+    if (!sort) return filteredData;
+    return sortBy(filteredData, sort);
+  }, [filter, sort, data]);
+
   return (
     <>
-      <div>this is users</div>
-      <ul>{users?.map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+      <FilterContainer
+        setFilter={setFilter}
+        filter={filter}
+        setSort={setSort}
+        sort={sort}
+      />
+
+      <div className={style.container}>
+        {modifiedUsers &&
+          modifiedUsers.map((user) => <UserCard key={user.id} user={user} />)}
+      </div>
     </>
   );
-}
+};
+
+Users.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
+
+export default Users;
