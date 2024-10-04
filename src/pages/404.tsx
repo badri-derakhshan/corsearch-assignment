deb7f53d4a193b6ced25a951f7eb3a125019520b
Baadrolzaman Derakhshan <badrolzaman.derakhshanboroujeni@gmail.com>
2024-10-05 00:32:54 +0300

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const Custom404 = () => {
+  return (
+    <main className='h-screen w-screen flex flex-col justify-center items-center'>
+      <h1>404 - Page Not Found</h1>
+      <Link href='/users' className='p-2 bg-'>
+        <Button>Go back to home</Button>
+      </Link>
+    </main>
+  );
+};
+
+export default Custom404;
